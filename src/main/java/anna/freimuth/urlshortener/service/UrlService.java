38f9de98bab192766b0d6a3cb443940eb8836f8a
Anna Freimuth <a.freimuth90@gmail.com>
2021-05-09 10:26:37 +0200

    removed host details from url service

--- src/main/java/anna/freimuth/urlshortener/service/UrlService.java
+++ src/main/java/anna/freimuth/urlshortener/service/UrlService.java
@@ -1,21 +1,19 @@
 package anna.freimuth.urlshortener.service;
 
 import anna.freimuth.urlshortener.dto.LongUrlDto;
-import anna.freimuth.urlshortener.dto.ShortUrlDto;
 import anna.freimuth.urlshortener.entity.Url;
 import anna.freimuth.urlshortener.helper.EncodeAndDecodeHelper;
 import anna.freimuth.urlshortener.repo.UrlRepo;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlService {
     private final UrlRepo urlRepo;
-    private final String HOST;
 
-    public UrlService( UrlRepo urlRepo, @Value("${host}") String host) {
+
+    public UrlService( UrlRepo urlRepo) {
         this.urlRepo = urlRepo;
-        this.HOST = host;
+
     }
 
     public String saveLongUrl(LongUrlDto longUrlDto){
