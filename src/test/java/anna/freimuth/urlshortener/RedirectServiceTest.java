d45b46282bef4dd07e88d903fbfb2f50b2f12b95
Anna Freimuth <a.freimuth90@gmail.com>
2021-05-24 21:42:49 +0200

    no redirection to expired link

--- src/test/java/anna/freimuth/urlshortener/RedirectServiceTest.java
+++ src/test/java/anna/freimuth/urlshortener/RedirectServiceTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @AutoConfigureMockMvc
@@ -22,25 +24,40 @@ public class RedirectServiceTest {
     UrlRepo urlRepo;
 
     @Test
-    void findLongUrl(){
+    void findLongUrl() {
 
         RedirectService redirectService = new RedirectService(urlRepo);
 
-        String shortUrl="do";
+        String shortUrl = "do";
         Long id = 103L;
-        String longUrlExpected="https://www.example.org";
+        String longUrlExpected = "https://www.example.org";
 
         Url tmpDatabaseObject = new Url();
         tmpDatabaseObject.setId(id);
         tmpDatabaseObject.setLongUrl(longUrlExpected);
         Optional<Url> databaseObject = Optional.of(tmpDatabaseObject);
 
-        when(urlRepo.findById(id)).thenReturn(databaseObject);
+        when(urlRepo.findNonExpiredById(id)).thenReturn(databaseObject);
 
         LongUrlDto response = redirectService.findLongUrl(shortUrl);
 
         assertEquals(longUrlExpected, response.getLongUrl());
         assertEquals(id, response.getId());
+    }
+
+    @Test
+    void expiredLongUrl() {
+
+        RedirectService redirectService = new RedirectService(urlRepo);
+
+        String shortUrl = "do";
+        Long id = 103L;
+
+        Optional<Url> databaseObject = Optional.empty();
+
+        when(urlRepo.findNonExpiredById(id)).thenReturn(databaseObject);
+
+        assertThrows(ResponseStatusException.class, () -> redirectService.findLongUrl(shortUrl));
     }
 }
 
