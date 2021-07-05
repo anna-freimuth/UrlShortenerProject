fec033ecb1122ae0e7b237cdde37116cc4d65e0e
Anna Freimuth <a.freimuth90@gmail.com>
2021-07-05 21:41:35 +0200

    changed statistics contoller

--- src/main/java/anna/freimuth/urlshortener/controller/StatisticsController.java
+++ src/main/java/anna/freimuth/urlshortener/controller/StatisticsController.java
@@ -2,31 +2,24 @@ package anna.freimuth.urlshortener.controller;
 
 import anna.freimuth.urlshortener.dto.StatisticDto;
 import anna.freimuth.urlshortener.service.StatisticsService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequestMapping("/statistics")
-@Controller
+@RestController
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
-    private final String host;
 
-    public StatisticsController(StatisticsService statisticsService, @Value("${host}") String host) {
+    public StatisticsController(StatisticsService statisticsService) {
         this.statisticsService = statisticsService;
-        this.host = host;
     }
 
     @GetMapping()
-    public String statistics(Model model){
-        List<StatisticDto> statistic = statisticsService.getTopCalled();
-        model.addAttribute("statistic", statistic);
-        model.addAttribute("host", host);
-        return "statistics";
+    public List<StatisticDto> statistics(){
+        return statisticsService.getTopCalled();
     }
 }
